f2c7ab86cad7e2e4c01dde7f474b2b10433c6d06
mkvesic <mk54121@fer.hr>
2023-10-28 16:26:23 +0200

    Initial commit

---
+++ Codeblaze/src/main/java/com/projektr/codeblaze/CodeblazeApplication.java
@@ -0,0 +1,21 @@
+package com.projektr.codeblaze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@SpringBootApplication
+public class CodeblazeApplication {
+
+	@Bean
+	public PasswordEncoder pswdEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodeblazeApplication.class, args);
+	}
+
+}
